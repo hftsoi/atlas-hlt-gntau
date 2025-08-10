762fd7f28859be22bcca24c7cf9da270d6159491
Ho-Fung Tsoi <hftsoi0411@gmail.com>
2025-08-10 23:57:02 +0200

    ntupler script

--- ntupler/do.sh
+++ ntupler/do.sh
@@ -1,25 +1,25 @@
 # mc23c
-plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.601191.PhPy8EG_AZNLO_Ztautau_v1_output.root -s /eos/user/h/htsoi/MxAODs/ntuple_Ztautau_PhPy8EG --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep_v1_output.root -s /eos/user/h/htsoi/MxAODs/ntuple_ttbar --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.700794.Sh_2214_Ztautau_maxHTpTV2_CVetoBVeto_v1_output.root -s /eos/user/h/htsoi/MxAODs/ntuple_Ztautau_Sh --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.801002.Py8EG_A14NNPDF23LO_Gammatautau_MassWeight_v1_output.root -s /eos/user/h/htsoi/MxAODs/ntuple_Gammatautau --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.801166.Py8EG_A14NNPDF23LO_jj_JZ1_v1_output.root -s /eos/user/h/htsoi/MxAODs/ntuple_JZ1 --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.801167.Py8EG_A14NNPDF23LO_jj_JZ2_v1_output.root -s /eos/user/h/htsoi/MxAODs/ntuple_JZ2 --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.801168.Py8EG_A14NNPDF23LO_jj_JZ3_v1_output.root -s /eos/user/h/htsoi/MxAODs/ntuple_JZ3 --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.801169.Py8EG_A14NNPDF23LO_jj_JZ4_v1_output.root -s /eos/user/h/htsoi/MxAODs/ntuple_JZ4 --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.601191.PhPy8EG_AZNLO_Ztautau_vmc23c_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/mc23c/Ztautau_PhPy --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep_vmc23c_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/mc23c/ttbar --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.700794.Sh_2214_Ztautau_maxHTpTV2_CVetoBVeto_vmc23c_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/mc23c/Ztautau_sherpa --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.801002.Py8EG_A14NNPDF23LO_Gammatautau_MassWeight_vmc23c_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/mc23c/Gtautau --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.801166.Py8EG_A14NNPDF23LO_jj_JZ1_vmc23c_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/mc23c/JZ1 --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.801167.Py8EG_A14NNPDF23LO_jj_JZ2_vmc23c_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/mc23c/JZ2 --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.801168.Py8EG_A14NNPDF23LO_jj_JZ3_vmc23c_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/mc23c/JZ3 --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/mc23c -i user.htsoi.TauID.801169.Py8EG_A14NNPDF23LO_jj_JZ4_vmc23c_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/mc23c/JZ4 --truth --rnnscore
 
 # pu200
-plant.py -m /eos/user/h/htsoi/MxAODs/pu200 -i user.htsoi.TauID.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep_v200_output.root -s /eos/user/h/htsoi/MxAODs/ntuple200_ttbar --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/pu200 -i user.htsoi.TauID.801002.Py8EG_A14NNPDF23LO_Gammatautau_MassWeight_v200_output.root -s /eos/user/h/htsoi/MxAODs/ntuple200_Gammatautau --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/pu200 -i user.htsoi.TauID.801167.Py8EG_A14NNPDF23LO_jj_JZ2_v200_output.root -s /eos/user/h/htsoi/MxAODs/ntuple200_JZ2 --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/pu200 -i user.htsoi.TauID.801168.Py8EG_A14NNPDF23LO_jj_JZ3_v200_output.root -s /eos/user/h/htsoi/MxAODs/ntuple200_JZ3 --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/pu200 -i user.htsoi.TauID.801169.Py8EG_A14NNPDF23LO_jj_JZ4_v200_output.root -s /eos/user/h/htsoi/MxAODs/ntuple200_JZ4 --truth --rnnscore
-#plant.py -m /eos/user/h/htsoi/MxAODs/pu200 -i  -s /eos/user/h/htsoi/MxAODs/ntuple200_ --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/pu200 -i user.htsoi.TauID.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep_vpu200_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/pu200/ttbar --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/pu200 -i user.htsoi.TauID.801002.Py8EG_A14NNPDF23LO_Gammatautau_MassWeight_vpu200_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/pu200/Gtautau --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/pu200 -i user.htsoi.TauID.801166.Py8EG_A14NNPDF23LO_jj_JZ1_vpu200_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/pu200/JZ1 --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/pu200 -i user.htsoi.TauID.801167.Py8EG_A14NNPDF23LO_jj_JZ2_vpu200_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/pu200/JZ2 --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/pu200 -i user.htsoi.TauID.801168.Py8EG_A14NNPDF23LO_jj_JZ3_vpu200_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/pu200/JZ3 --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/pu200 -i user.htsoi.TauID.801169.Py8EG_A14NNPDF23LO_jj_JZ4_vpu200_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/pu200/JZ4 --truth --rnnscore
 
 # pu140
-plant.py -m /eos/user/h/htsoi/MxAODs/pu140 -i user.htsoi.TauID.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep_v140_output.root -s /eos/user/h/htsoi/MxAODs/ntuple140_ttbar --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/pu140 -i user.htsoi.TauID.801002.Py8EG_A14NNPDF23LO_Gammatautau_MassWeight_v140_output.root -s /eos/user/h/htsoi/MxAODs/ntuple140_Gammatautau --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/pu140 -i user.htsoi.TauID.801166.Py8EG_A14NNPDF23LO_jj_JZ1_v140_output.root -s /eos/user/h/htsoi/MxAODs/ntuple140_JZ1 --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/pu140 -i user.htsoi.TauID.801167.Py8EG_A14NNPDF23LO_jj_JZ2_v140_output.root -s /eos/user/h/htsoi/MxAODs/ntuple140_JZ2 --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/pu140 -i user.htsoi.TauID.801168.Py8EG_A14NNPDF23LO_jj_JZ3_v140_output.root -s /eos/user/h/htsoi/MxAODs/ntuple140_JZ3 --truth --rnnscore
-plant.py -m /eos/user/h/htsoi/MxAODs/pu140 -i user.htsoi.TauID.801169.Py8EG_A14NNPDF23LO_jj_JZ4_v140_output.root -s /eos/user/h/htsoi/MxAODs/ntuple140_JZ4 --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/pu140 -i user.htsoi.TauID.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep_vpu140_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/pu140/ttbar --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/pu140 -i user.htsoi.TauID.801002.Py8EG_A14NNPDF23LO_Gammatautau_MassWeight_vpu140_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/pu140/Gtautau --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/pu140 -i user.htsoi.TauID.801166.Py8EG_A14NNPDF23LO_jj_JZ1_vpu140_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/pu140/JZ1 --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/pu140 -i user.htsoi.TauID.801167.Py8EG_A14NNPDF23LO_jj_JZ2_vpu140_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/pu140/JZ2 --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/pu140 -i user.htsoi.TauID.801168.Py8EG_A14NNPDF23LO_jj_JZ3_vpu140_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/pu140/JZ3 --truth --rnnscore
+plant.py -m /eos/user/h/htsoi/MxAODs/pu140 -i user.htsoi.TauID.801169.Py8EG_A14NNPDF23LO_jj_JZ4_vpu140_output.root -s /eos/user/h/htsoi/MxAODs/ntuples/pu140/JZ4 --truth --rnnscore
